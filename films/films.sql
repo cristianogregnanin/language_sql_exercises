6d8ec89e4df5ecc4e076cac128d2770f660d2ab6
cristianogregnanin <cristiano.gregnanin@gmail.com>
2019-03-25 11:50:03 -0700

    films

--- films/films.sql
+++ films/films.sql
@@ -47,6 +47,11 @@ insert into sale (posti, nome, citta) values (16, 'sala2', 'Pisa');
 insert into sale (posti, nome, citta) values (164, 'sala3', 'Pisa');
 insert into sale (posti, nome, citta) values (123, 'sala1', 'Ferrara');
 insert into sale (posti, nome, citta) values (151, 'sala1', 'Bologna');
+insert into sale (posti, nome, citta) values (140, 'sala1', 'Napoli');
+
+insert into attori (nome, anno_di_nascita, nazionalita) values ('R.williams', '1960', 'Americana');
+insert into attori (nome, anno_di_nascita, nazionalita) values ('S.loren', '1940', 'italiana');
+insert into attori (nome, anno_di_nascita, nazionalita) values ('M.mastroianni', '1950', 'italiana');
 
 insert into films (titolo, anno_di_produzione,nazionalita, regista, genere)
 values ('film 1', '1986', 'italiana', 'fellini', 'storico');
@@ -77,10 +82,71 @@ insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
 values (3, 2, 1000, '2017/12/25');
 insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
 values (2, 1, 1000, '2004/12/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (4, 6, 1000, '2004/12/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (1, 6, 1000, '2004/12/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (1, 6, 1000, '2004/12/5');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (2, 5, 1000, '2004/3/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (3, 5, 1000, '2004/4/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (4, 4, 1000, '2004/1/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (5, 3, 1000, '2004/6/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (6, 3, 1000, '2004/11/25');
+insert into proiezioni (film_id, sala_id, incasso, data_proiezione)
+values (7, 3, 1000, '2004/10/25');
+
+
+insert into recite (film_id, attore_id) values (1,1);
+insert into recite (film_id, attore_id) values (2,2);
+insert into recite (film_id, attore_id) values (3,2);
+insert into recite (film_id, attore_id) values (4,2);
+insert into recite (film_id, attore_id) values (5,3);
+insert into recite (film_id, attore_id) values (6,2);
+insert into recite (film_id, attore_id) values (6,3);
+insert into recite (film_id, attore_id) values (7,2);
+insert into recite (film_id, attore_id) values (7,3);
+
+
 
 select titolo from films where regista = (select regista from films where titolo = 'Casablanca');
 
-select films.titolo, films.genere from films, proiezioni where  proiezioni.film_id = films.id and data_proiezione = '2004-12-25';
+select films.titolo, films.genere from films, proiezioni where proiezioni.film_id = films.id and data_proiezione = '2004-12-25';
+
+select films.titolo, films.genere
+from films, proiezioni, sale
+where sale.citta = 'Napoli' 
+and proiezioni.film_id = films.id
+and sale.id = proiezioni.sala_id
+and data_proiezione = '2004-12-25';
+
+select sale.nome
+from attori, recite, films, proiezioni, sale
+where proiezioni.sala_id = sale.id
+and proiezioni.film_id = films.id 
+and films.id = recite.film_id
+and attori.id = recite.attore_id
+and attori.nome = 'R.Williams'
+and sale.citta = 'Napoli'
+and data_proiezione = '2004-12-25';
+
+select distinct films.titolo, films.nazionalita, films.regista
+from films, recite, attori
+where films.id = recite.film_id
+and attori.id = recite.attore_id
+and (attori.nome = 'M.mastroianni' or attori.nome = 'S.loren');
+
+select films.titolo from films
+where 'M.mastroianni' in 
+(select attori.nome from attori,recite where films.id = recite.film_id and recite.attore_id = attori.id)
+and 'S.loren' in 
+(select attori.nome from attori,recite where films.id = recite.film_id and recite.attore_id = attori.id);
+ 
 
 
 
